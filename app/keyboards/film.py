eac207a282cf1f4de9244eb88c094edb35b006df
mollygin <yevartemivna.rul@gmail.com>
2024-03-24 18:47:19 +0100

    proper code updated

--- app/keyboards/film.py
+++ app/keyboards/film.py
@@ -1,7 +1,5 @@
 from aiogram.utils.keyboard import InlineKeyboardBuilder
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
+from aiogram.types import ReplyKeyboardMarkup
 def build_films_keyboard(films: list):
     builder = InlineKeyboardBuilder()
     for index, film in enumerate(films):
@@ -13,16 +11,12 @@ def build_film_details_keyboard(url):
     builder = InlineKeyboardBuilder()
     builder.button(text="Посилання на сайт: ", url=url)
     builder.button(text="Назад", callback_data="back")
-    #builder.button(text="Сайт: ", url="https://brain-school.com.ua/category/formuly-fizyka/")
+    #builder.button(text="Сайт з формулами: ", url="https://brain-school.com.ua/category/formuly-fizyka/")
     return builder.as_markup()
 
 def build_menu_keyboard():
-    #keyboard = InlineKeyboardMarkup()
-    #link_button = InlineKeyboardButton(text="Сайт з інформацією: ", url="https://brain-school.com.ua/category/formuly-fizyka/")
-    #keyboard.add(link_button)
-    #return keyboard
-    keyboard = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-    link_button = InlineKeyboardButton(text="Сайт з інформацією: ",
-                                       url="https://brain-school.com.ua/category/formuly-fizyka/")
-    keyboard.add(link_button)
-    return keyboard
+    builder = ReplyKeyboardMarkup()
+    builder.button(text="Посилання на сайт: ", callback_data="https://brain-school.com.ua/category/formuly-fizyka/")
+    builder.button(text="Формули", callback_data=f"/formulas")
+    builder.button(text="Створити нову формулу", callback_data=f"/createform")
+    return builder.as_markup()
